9f9a88510a6ee0a1fec622a270431fd0a2ccc341
rafaelseifert <rafaelseifert93@gmail.com>
2019-12-14 13:39:25 +0100

    Refactor overview, set communication between sibling-components and add vertical ruler

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,65 @@
 <template>
   <div>
     <h1>Job Page</h1>
-    <div class="columns">
+    <main class="content-container columns">
       <section>
-        <job-overview />
+        <h2>Job Overview</h2>
+        <ul>
+          <job-item
+            v-for="job in jobs"
+            :key="job.id"
+            :job="job"
+            @setActiveJob="setActiveJob($event)"
+          />
+        </ul>
       </section>
       <section>
-        <job-detail />
+        <job-detail :activeJob="activeJob" v-if="activeJob !== null" />
       </section>
-    </div>
+    </main>
   </div>
 </template>
 
 <script>
-import JobOverview from "../components/JobOverview";
+import mock from "../api/mock";
+import JobItem from "../components/JobItem";
 import JobDetail from "../components/JobDetail";
 
 export default {
   name: "home",
   components: {
-    JobOverview: JobOverview,
+    JobItem: JobItem,
     JobDetail: JobDetail
+  },
+  data() {
+    return {
+      jobs: mock.jobs,
+      activeJob: null
+    };
+  },
+  methods: {
+    setActiveJob(id) {
+      this.activeJob = id;
+    }
   }
 };
 </script>
 
 <style scoped lang="scss">
+.content-container {
+  position: relative;
+  &::after {
+    content: "";
+    position: absolute;
+    left: 50%;
+    top: 0;
+    display: block;
+    height: 100%;
+    width: 1px;
+    background: #eeeeee;
+  }
+}
+
 .columns {
   @media screen and (min-width: 1024px) {
     display: flex;
