d4a596fd48465b29152181555083bbdd7611f17c
rafaelseifert <rafaelseifert93@gmail.com>
2019-12-14 13:36:12 +0100

    Adds global truncate-filter

--- src/main.js
+++ src/main.js
@@ -4,6 +4,16 @@ import router from "./router";
 
 Vue.config.productionTip = false;
 
+const truncateFilter = function(text, length, clamp) {
+  clamp = clamp || "...";
+  let node = document.createElement("div");
+  node.innerHTML = text;
+  let content = node.textContent;
+  return content.length > length ? content.slice(0, length) + clamp : content;
+};
+
+Vue.filter("truncate", truncateFilter);
+
 new Vue({
   router,
   render: h => h(App)
