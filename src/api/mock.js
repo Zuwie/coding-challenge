fc9d6220adf0867350f3d62905e1a1a86b21b479
rafaelseifert <rafaelseifert93@gmail.com>
2019-12-14 13:35:39 +0100

    Adjusts mock-data to better differentiate between them and properly start with correct id

--- src/api/mock.js
+++ src/api/mock.js
@@ -1,27 +1,27 @@
 const jobs = [
   {
-    id: 1,
+    id: 0,
     title: "Frontend Engineer",
     type: "Full Time",
     company: "Prescreen",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aliquid, animi at consectetur, debitis dicta eaque eius excepturi illum itaque iusto minima neque nesciunt numquam placeat ratione repellendus sint tempore."
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem delectus dolores enim fugit ipsum nisi nobis odit quis repellat ullam. Delectus doloremque, est et impedit, inventore nam neque nostrum odit perspiciatis quae quas quisquam sapiente? Ab blanditiis commodi, culpa eum ex minus officia porro praesentium quia reprehenderit! Blanditiis commodi corporis cum dolor est explicabo iure maiores minus, modi, molestiae, nisi nobis praesentium quam quod recusandae sapiente voluptas."
+  },
+  {
+    id: 1,
+    title: "Pre Sales Manager_Prescreen",
+    type: "Part Time",
+    company: "Prescreen",
+    description:
+      "Aliquam molestias nam non, quae qui recusandae voluptatum? Aliquid cumque dolores et expedita, magni vitae voluptatibus. Delectus libero molestiae quo temporibus! Adipisci amet animi aut beatae deleniti dicta dolorum ducimus enim facilis impedit maiores maxime necessitatibus nesciunt nobis odit officia provident quas quasi quod, quos ratione repudiandae saepe veritatis? Consectetur consequatur modi necessitatibus nemo porro quia ratione sequi. Architecto ex, illo. A atque deserunt nihil possimus rem? Architecto aspernatur cum doloribus earum nam quaerat, quas ut voluptatibus."
   },
   {
     id: 2,
-    title: "Pre Sales Manager_Prescreen",
-    type: "Full Time",
-    company: "Prescreen",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aliquid, animi at consectetur, debitis dicta eaque eius excepturi illum itaque iusto minima neque nesciunt numquam placeat ratione repellendus sint tempore."
-  },
-  {
-    id: 3,
     title: "Performance Marketing Manager",
-    type: "Full Time",
+    type: "Internship",
     company: "Prescreen",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aliquid, animi at consectetur, debitis dicta eaque eius excepturi illum itaque iusto minima neque nesciunt numquam placeat ratione repellendus sint tempore."
+      "Dolore ea fuga hic laboriosam repellendus rerum tempora vitae! Alias perferendis quae recusandae! Accusantium aperiam aut earum provident recusandae! A architecto aspernatur, delectus deleniti dolore, ea error et eveniet excepturi, facere fugit in iste iusto natus nobis quis ratione reprehenderit sapiente sunt velit? Blanditiis, cum dolor incidunt ipsam libero placeat quo. A ab, ad aliquam animi consequuntur, cupiditate error id impedit incidunt laboriosam magnam, magni modi nostrum porro quasi rem sequi? Adipisci aliquam beatae culpa, exercitationem libero nulla numquam quam reiciendis rerum. Dicta distinctio dolores exercitationem fuga fugiat hic magnam obcaecati possimus reprehenderit, rerum, saepe sed totam veritatis."
   }
 ];
 
