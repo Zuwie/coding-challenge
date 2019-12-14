afc7a6d55b13fa3c206259c6d9cbce07688b66ee
rafaelseifert <rafaelseifert93@gmail.com>
2019-12-14 13:37:36 +0100

    Properly display its data and adds basic styles

--- src/components/JobItem.vue
+++ src/components/JobItem.vue
@@ -1,8 +1,16 @@
 <template>
-  <div>
-    <h3>
+  <div class="jobitem">
+    <h3 class="jobitem__title">
       {{ job.title }}
     </h3>
+    <p class="jobitem__type">Type: {{ job.type }}</p>
+    <p class="jobitem__company">Company: {{ job.company }}</p>
+    <p class="jobitem__description">
+      {{ job.description | truncate(50) }}
+    </p>
+    <button class="button" @click="setActiveJob(job.id)">
+      More information about this job
+    </button>
   </div>
 </template>
 
@@ -14,6 +22,10 @@ export default {
       type: Object,
       default() {
         return {
+          id: {
+            type: Number,
+            default: null
+          },
           title: {
             type: String,
             default: "Job Title"
@@ -33,8 +45,44 @@ export default {
         };
       }
     }
+  },
+  methods: {
+    setActiveJob(id) {
+      this.$emit("setActiveJob", id);
+    }
   }
 };
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.jobitem {
+  padding: 20px;
+  margin-bottom: 20px;
+  box-shadow: rgba(0, 0, 0, 0.15) 0 8px 16px 0;
+  border-radius: 5px;
+
+  &__description {
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+}
+
+.button {
+  cursor: pointer;
+  background: #ffffff;
+  box-shadow: none;
+  outline: none;
+  padding: 11px 20px 12px;
+  border: 1px solid #000000;
+  text-transform: uppercase;
+  font-weight: 700;
+  font-size: 12px;
+  line-height: 1.3;
+  transition: 0.2s ease;
+
+  &:hover {
+    color: #fff;
+    background: #000;
+  }
+}
+</style>
