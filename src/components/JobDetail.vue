4de3306a10c186657ac92a92f959e903edbcb5e3
rafaelseifert <rafaelseifert93@gmail.com>
2019-12-14 13:38:12 +0100

    Display data of active job

--- src/components/JobDetail.vue
+++ src/components/JobDetail.vue
@@ -1,12 +1,39 @@
 <template>
   <div>
     <h2>Detail</h2>
+
+    <div class="jobdetails">
+      <h3 class="jobitem__title">
+        {{ jobDetails.title }}
+      </h3>
+      <p class="jobitem__type">Type: {{ jobDetails.type }}</p>
+      <p class="jobitem__company">Company: {{ jobDetails.company }}</p>
+      <p class="jobitem__description">{{ jobDetails.description }}</p>
+    </div>
   </div>
 </template>
 
 <script>
+import mock from "../api/mock";
+
 export default {
-  name: "JobDetail"
+  name: "JobDetail",
+  props: {
+    activeJob: {
+      type: Number,
+      default: null
+    }
+  },
+  data() {
+    return {
+      jobs: mock.jobs
+    };
+  },
+  computed: {
+    jobDetails() {
+      return this.jobs[this.activeJob];
+    }
+  }
 };
 </script>
 
