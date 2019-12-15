a031a3648d0574f9c863a806aa70a7cc68c367f6
rafaelseifert <rafaelseifert93@gmail.com>
2019-12-15 18:16:55 +0100

    Refactor navigation to its own component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <div id="nav" class="inner">
-      <ul class="nav-list">
-        <li><router-link to="/">Home</router-link></li>
-        <li><router-link to="/about">About</router-link></li>
-        <li><router-link to="/contact">Contact</router-link></li>
-      </ul>
-    </div>
+    <the-header></the-header>
     <main class="content-container inner">
       <router-view />
     </main>
   </div>
 </template>
+
+<script>
+import TheHeader from "@/components/TheHeader";
+
+export default {
+  name: "App",
+  components: {
+    TheHeader
+  }
+};
+</script>
 
 <style lang="scss">
 body,
@@ -31,27 +36,6 @@ ul {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-.nav-list {
-  display: flex;
-
-  li + li {
-    margin-left: 20px;
-  }
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 
 .inner {
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <header>
+    <nav id="nav" class="inner">
+      <ul class="nav-list">
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/about">About</router-link></li>
+        <li><router-link to="/contact">Contact</router-link></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "TheHeader"
+};
+</script>
+
+<style scoped lang="scss">
+.nav-list {
+  display: flex;
+  padding: 30px 20px;
+
+  li + li {
+    margin-left: 20px;
+  }
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
